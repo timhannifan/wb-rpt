1bea5585ffc3bc50568f75b326d10bac99255372
timhannifan <hannifan@gmail.com>
2016-04-11 14:35:08 -0400

    rework all rpt to be based off initial insert, removed all after.update collection hooks

--- server/insertRpt.js
+++ server/insertRpt.js
@@ -88,143 +88,130 @@ insertRpt: function( data) {
 
 Rpt.after.insert(function (userId, doc) {
 	//// ADDING CLEANTITLE, MAPTOFOUR, KEYWORDS TO TITLETAGS AND TAGSONLY
-		Rpt.update({_id: this._id}, {$set: 
-		{
+	Rpt.update({_id: this._id}, { $set: {
 			cleanTitle: cleanUp(doc.occ_title),
 			cleanDesc: cleanUp(doc.occ_desc),
 			cleanSector: cleanUp(doc.sector),
 			titleTags: _.uniq(yakiSplitClean(cleanUp(doc.occ_title))),
 			tagsOnly: _.uniq(yakiSplitClean(cleanUp(doc.occ_title))),
 			descTags: _.uniq(yakiSplitClean(cleanUp(doc.occ_desc))),
+	}}, function (err,res) {
+		if (err) {
+			console.log(err);
+		} else {
+
+			/// if title has not been added to titleTags, add it, then mark parsing process complete
+			if(!doc.parsingComplete) {
+				Rpt.update({_id: doc._id}, {
+					$push: {
+						titleTags: doc.cleanTitle,
+					},
+					$set: {
+						parsingComplete: 1
+					}
+				}, function (err,res) {
+					if (err) {console.log(err)} else {console.log('parsing process complete')}
+				});		
+				var parsedDoc = Rpt.findOne({_id:doc._id});
+				/// parsing is complete, start intersection work
+				if(parsedDoc.parsingComplete){
+					/// if no title intersection work done, add title _ids in bulk, then mark bulkIntersectionTitleComplete complete
+					if(!parsedDoc.bulkIntersectionTitleComplete) {
+						var matches = MascoKey.find({keywords: {$in: parsedDoc.titleTags}}).fetch();
+						var matchIdSet = [];
+						for (var i = matches.length - 1; i >= 0; i--) {
+							matchIdSet.push(matches[i]._id);
+						}
+
+						var uniques = _.uniq(matchIdSet);
+
+						Rpt.update({_id: parsedDoc._id}, {
+							$push: {
+								titleIntersections: {
+									$each: uniques
+								}
+							},
+							$set: {
+								bulkIntersectionTitleComplete: 1
+							}
+						});
+						/// bulk title _ids have been added, find detail, then mark intersectionTitleDetailComplete complete
+						var intersectionsTitleDoc =  Rpt.findOne({_id:doc._id});
+						var matches = intersectionsTitleDoc.titleIntersections;
+						var matchDetail = [];
+
+						for (var i = matches.length - 1; i >= 0; i--) {
+							var matchDetailItem = {};
+							var matchingMasco = MascoKey.findOne({_id: matches[i]});
+
+							matchDetailItem.mascoCode = matchingMasco.officialCode;
+						  matchDetailItem.mascoKeywordLength = matchingMasco.keywords.length;
+						  matchDetailItem.rptArrayLength = intersectionsTitleDoc.titleTags.length;
+						  matchDetailItem.intersectionValue = findIntersection(matchingMasco.keywords,intersectionsTitleDoc.titleTags);
+						  matchDetailItem.percentVsMascoSize = matchDetailItem.intersectionValue.length/matchingMasco.keywords.length;
+						  matchDetailItem.percentVsRptSize = matchDetailItem.intersectionValue.length/intersectionsTitleDoc.titleTags.length;
+
+						  matchDetail.push(matchDetailItem);
+						}
+
+						Rpt.update({_id: intersectionsTitleDoc._id}, {
+							$set: {
+								intersectionTitleDetailComplete: 1,
+								titleIntersectionDetail: matchDetail
+							}
+						});
+
+					}
+					/// if no desc intersection work done, add desc _ids in bulk, , then mark bulkIntersectionDescComplete complete
+					if(!parsedDoc.bulkIntersectionDescComplete) {
+
+						var matches = MascoKey.find({keywords: {$in: parsedDoc.cleanDescTags}}).fetch();
+						var matchIdSet = [];
+						for (var i = matches.length - 1; i >= 0; i--) {
+							matchIdSet.push(matches[i]._id);
+						}
+
+						var uniques = _.uniq(matchIdSet);
+
+						Rpt.update({_id: parsedDoc._id}, {
+							$push: {
+								descIntersections: {
+									$each: uniques
+								}
+							},
+							$set: {
+								bulkIntersectionDescComplete: 1
+							}
+						});
+						
+						/// bulk title _ids have been added, find detail, then mark intersectionDescDetailComplete complete
+						var intersectionsDescDoc =  Rpt.findOne({_id:doc._id});
+						var matches = intersectionsDescDoc.titleIntersections;
+						var matchDetail = [];
+
+						for (var i = matches.length - 1; i >= 0; i--) {
+							var matchDetailItem = {};
+							var matchingMasco = MascoKey.findOne({_id: matches[i]});
+
+							matchDetailItem.mascoCode = matchingMasco.officialCode;
+						  matchDetailItem.mascoKeywordLength = matchingMasco.keywords.length;
+						  matchDetailItem.rptArrayLength = intersectionsDescDoc.cleanDescTags.length;
+						  matchDetailItem.intersectionValue = findIntersection(matchingMasco.keywords,intersectionsDescDoc.cleanDescTags);
+						  matchDetailItem.percentVsMascoSize = matchDetailItem.intersectionValue.length/matchingMasco.keywords.length;
+						  matchDetailItem.percentVsRptSize = matchDetailItem.intersectionValue.length/intersectionsDescDoc.cleanDescTags.length;
+
+						  matchDetail.push(matchDetailItem);
+						}
+
+						Rpt.update({_id: parsedDoc._id}, {
+							$set: {
+								intersectionDescDetailComplete: 1,
+								descIntersectionDetail: matchDetail
+							}
+						});	
+					}
+				}
+			}
 		}
-	}, function (err,res) {
-		if (err) {console.log(err)} else {console.log('RPT update successful')}
 	});
 });
-
-Rpt.after.update(function (userId, doc, fieldNames, modifier, options) {
-	if(!doc.parsingComplete) {
-		Rpt.update({_id: doc._id}, {
-			$push: {
-				titleTags: doc.cleanTitle,
-			},
-			$set: {
-				parsingComplete: 1
-			}
-		}, function (err,res) {
-			if (err) {console.log(err)} else {console.log('titleTags updated successfully')}
-		});		
-	}
-
-	if(doc.parsingComplete && !doc.bulkIntersectionTitleComplete) {
-		var matches = MascoKey.find({keywords: {$in: doc.titleTags}}).fetch();
-		var matchIdSet = [];
-		for (var i = matches.length - 1; i >= 0; i--) {
-			matchIdSet.push(matches[i]._id);
-		}
-
-		var uniques = _.uniq(matchIdSet);
-
-		console.log('uniques count' + uniques.length);
-
-		Rpt.update({_id: doc._id}, {
-			$push: {
-				titleIntersections: {
-					$each: uniques
-				}
-			},
-			$set: {
-				bulkIntersectionTitleComplete: 1
-			}
-		}, function (err,res) {
-			if (err) {console.log(err)} else {console.log('bulkTitle intersection done')}
-		});
-	}
-	if(doc.parsingComplete && !doc.bulkIntersectionDescComplete) {
-		var matches = MascoKey.find({keywords: {$in: doc.cleanDescTags}}).fetch();
-		var matchIdSet = [];
-		for (var i = matches.length - 1; i >= 0; i--) {
-			matchIdSet.push(matches[i]._id);
-		}
-
-		var uniques = _.uniq(matchIdSet);
-
-		console.log('uniques count' + uniques.length);
-
-		Rpt.update({_id: doc._id}, {
-			$push: {
-				descIntersections: {
-					$each: uniques
-				}
-			},
-			$set: {
-				bulkIntersectionDescComplete: 1
-			}
-		}, function (err,res) {
-			if (err) {console.log(err)} else {console.log('bulkIntersectionDescComplete')}
-		});
-	}
-
-	if(doc.parsingComplete && doc.bulkIntersectionTitleComplete && !doc.intersectionTitleDetailComplete) {
-
-		var matches = doc.titleIntersections;
-		var matchDetail = [];
-
-		for (var i = matches.length - 1; i >= 0; i--) {
-			var matchDetailItem = {};
-			var matchingMasco = MascoKey.findOne({_id: matches[i]});
-
-			matchDetailItem.mascoCode = matchingMasco.officialCode;
-		  matchDetailItem.mascoKeywordLength = matchingMasco.keywords.length;
-		  matchDetailItem.rptArrayLength = doc.titleTags.length;
-		  matchDetailItem.intersectionValue = findIntersection(matchingMasco.keywords,doc.titleTags);
-		  matchDetailItem.percentVsMascoSize = matchDetailItem.intersectionValue.length/matchingMasco.keywords.length;
-		  matchDetailItem.percentVsRptSize = matchDetailItem.intersectionValue.length/doc.titleTags.length;
-
-		  console.log('matchDetailItem '+ i, matchDetailItem);
-		  matchDetail.push(matchDetailItem);
-		}
-		
-		console.log(matchDetail.length + 'items in matchDetail array');
-
-		Rpt.update({_id: doc._id}, {
-			$set: {
-				intersectionTitleDetailComplete: 1,
-				titleIntersectionDetail: matchDetail
-			}
-		}, function (err,res) {
-			if (err) {console.log(err)} else {console.log('intersectionTitleDetailComplete')}
-		});
-	}
-	if(doc.parsingComplete && doc.bulkIntersectionDescComplete && !doc.intersectionDescDetailComplete) {
-
-		var matches = doc.descIntersections;
-		var matchDetail = [];
-
-		for (var i = matches.length - 1; i >= 0; i--) {
-			var matchDetailItem = {};
-			var matchingMasco = MascoKey.findOne({_id: matches[i]});
-
-			matchDetailItem.mascoCode = matchingMasco.officialCode;
-		  matchDetailItem.mascoKeywordLength = matchingMasco.keywords.length;
-		  matchDetailItem.rptArrayLength = doc.cleanDescTags.length;
-		  matchDetailItem.intersectionValue = findIntersection(matchingMasco.keywords,doc.cleanDescTags);
-		  matchDetailItem.percentVsMascoSize = matchDetailItem.intersectionValue.length/matchingMasco.keywords.length;
-		  matchDetailItem.percentVsRptSize = matchDetailItem.intersectionValue.length/doc.cleanDescTags.length;
-
-		  console.log('matchDetailItem '+ i, matchDetailItem);
-		  matchDetail.push(matchDetailItem);
-		}
-		
-		console.log(matchDetail.length + 'items in matchDetail array');
-
-		Rpt.update({_id: doc._id}, {
-			$set: {
-				intersectionDescDetailComplete: 1,
-				descIntersectionDetail: matchDetail
-			}
-		}, function (err,res) {
-			if (err) {console.log(err)} else {console.log('intersectionDescDetailComplete')}
-		});
-	}	
-}, {fetchPrevious: false});
